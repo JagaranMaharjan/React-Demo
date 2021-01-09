dc8e485e509111f37e6c756726afa810207b7f97
JagaranMaharjan <01jagaranmah10@gmail.com>
2021-01-09 18:00:00 +0545

    intsall package react-data-table-component, create UI in enrollment.js file and implement datatable by fecthing fake data.

--- src/containers/enrollment.js
+++ src/containers/enrollment.js
@@ -1,13 +1,32 @@
 import {Component} from 'react';
+import DataTable, { createTheme } from 'react-data-table-component';
+
+createTheme('solarized', {
+    text: {
+        primary: '#268bd2',
+        secondary: '#2aa198',
+    },
+    background: {
+        default: '#002b36',
+    },
+    context: {
+        background: '#cb4b16',
+        text: '#FFFFFF',
+    },
+    divider: {
+        default: '#073642',
+    },
+    action: {
+        button: 'rgba(0,0,0,.54)',
+        hover: 'rgba(0,0,0,.08)',
+        disabled: 'rgba(0,0,0,.12)',
+    },
+});
+
 export default class Enrollment extends Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            'name':props.name,
-            'email':props.email,
-            'age':props.age
-        }
-    }
+   state = {
+       data: []
+   }
 
     handleChange=(event, field)=>{
         this.setState({
@@ -15,30 +34,78 @@ export default class Enrollment extends Component{
         });
     }
 
-    handleOnSubmit=(_)=>{
-        console.log(this.state.name, this.state.email, this.state.age);
+    
+    handleOnSubmit = (_) => {
+        console.log(this.state.group_name, this.state.level_name, this.state.semester_name);
+    }
+
+    async componentDidMount(){
+        const response = await fetch('https://jsonplaceholder.typicode.com/todos/');
+        const data = await response.json();
+        this.setState({
+            data: data,
+        });
     }
 
     render(){
+        //const data = [{ id: 1, title: 'Conan the Barbarian', year: '1982' }];
+        const columns = [
+            {
+                name: 'userId',
+                selector: 'userId',
+                sortable: true,
+            },
+            {
+                name: 'title',
+                selector: 'title',
+                sortable: true,
+                right: true,
+            },
+            {
+                name: 'completed',
+                selector: 'completed',
+                sortable: true,
+                right: true,
+            },
+            {
+                name: 'id',
+                selector: 'id',
+                sortable: true,
+                right: true,
+            },
+        ];
         return (
+            
             <div className="container">
-                <div className="col-md-6">
-                    <form>
-                        <div className="form-group">
-                            <label for="exampleInputName1">Name</label>
-                            <input type="name" className="form-control" placeholder="Enter name" onChange={(e)=>this.handleChange(e, "name")} value={this.state.name} />
-                        </div>
-                        <div className="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" className="form-control" placeholder="Enter email" onChange={(e) => this.handleChange(e, "email")} value={this.state.email} />
-                            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                        </div>
-                        <div className="form-group">
-                            <label for="exampleInputAge1">Age</label>
-                                <input type="number" className="form-control" id="exampleInputAge1" placeholder="Enter age" onChange={(e) => this.handleChange(e, "age")} value={this.state.age}/>
-                        </div>
-                        <button type="submit" className="btn btn-primary" onClick={this.handleOnSubmit}>Submit</button>
-                    </form>
+                <br></br>
+                <div className="row">
+                    <div className="col-md-3">
+                        <form>
+                            <div className="form-group">
+                                <label for="exampleInputName1">Name</label>
+                                <input type="name" className="form-control" placeholder="Enter group name" onChange={(e) => this.handleChange(e, "group_name")} value={this.state.group_name} />
+                            </div>
+                            <div className="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="text" className="form-control" placeholder="Enter semester name" onChange={(e) => this.handleChange(e, "semester_name")} value={this.state.semester_name} />
+
+                            </div>
+                            <div className="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="text" className="form-control" placeholder="Enter group name" onChange={(e) => this.handleChange(e, "level_name")} value={this.state.level_name} />
+
+                            </div>
+                            <button type="submit" className="btn btn-primary" onClick={this.handleOnSubmit}>Submit</button>
+                        </form>
+                    </div>
+                    <div className="col-md-9">
+                    {console.log(this.state.data)}
+                        <DataTable
+                            title="Group Details"
+                            columns={columns}
+                            data={this.state.data}                           
+                        />
+                    </div>
                 </div>
             </div>
         );
